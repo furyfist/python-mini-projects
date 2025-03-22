2fbf611db9223e920b5054f951512cb63ab85ee0
Himanshu <himanshu290304@gmail.com>
2025-03-22 15:14:08 +0530

    completed dice roll project

--- dice_roll.py
+++ dice_roll.py
@@ -1,13 +1,30 @@
+#LOOP
+    # always map out whats you going to do
+    # Ask: roll a dice?
+    # if user enters y -> generates two random numbers
+    # if user entes n -> print thank you message
+    # else -> Invalid Choice
+
+# Optimal Enhancements 
+#   1. specify how many dice user wants to roll.
+#   2. Feature Keeps track of how many time the user has rolled the dice during the session.
+
 import random
 
-ans = input("Roll the dice? (y/n): ")
-
-if ans == "n" or ans == "N":
-    print("Thanks for Playing!")
-elif ans =="y" or ans =="Y":
-    response = random.randint(1,6)
-    response2 = random.randint(1,6)
-    print(f"you rolled : {response} and {response2}")
-else:
-    print("Invalid Choice")
+cnt = int(input("How many times you wants to roll the dice? : "))
+total = 0
+while cnt > 0:
+    print(f"Total times dice rolled : {total} ")
     
+    cnt-=1
+    ans = input("Roll the dice? (y/n): ").lower()
+    if ans == "n":
+        print("Thanks for Playing!")
+        break
+    elif ans =="y":
+        total+=1
+        response = random.randint(1,6)
+        response2 = random.randint(1,6)
+        print(f"({response},{response2})")
+    else:
+        print("Invalid Choice")
